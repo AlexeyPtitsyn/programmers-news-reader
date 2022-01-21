6574ef4096aed9f9a9517380c317322152f131a4
Alexey Ptitsyn <alexey.ptitsyn@gmail.com>
2022-01-21 14:00:00 +0300

    Added IndexedDB backend for options page.

--- src/options/components/MainComponent.jsx
+++ src/options/components/MainComponent.jsx
@@ -6,12 +6,59 @@
 
 import React, { useEffect, useState } from 'react';
 
+import DB from '../db.js';
 import './MainComponent.scss';
 
 function MainComponent() {
+  const [list, setList] = useState([]);
+
+  const updateList = async () => {
+    const data = await DB.getList();
+    setList(data);
+  };
+
+  useEffect(() => {
+    updateList();
+  }, []);
+
+
+  const listItems = list.map((item) => {
+    return (
+      <div key={ item.id } className="main-component__list-item">
+        { item.name }
+      </div>
+    );
+  });
+
   return (
     <div className="main-component">
-      [options]
+      <div className="main-component__left">
+        { listItems }
+      </div>
+      <div className="main-component__right">
+
+        <div>
+          <label>
+            Name:
+            <input type="text" value={0} onChange={()=>{}} />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            URL:
+            <input type="text" value={0} onChange={()=>{}} />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Parse function (TODO: instructions):
+            <textarea value={0} onChange={() => {}}></textarea>
+          </label>
+        </div>
+
+      </div>
     </div>
   );
 }
